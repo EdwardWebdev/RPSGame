a4b9734f74803739e6bdebdce4325f8d1401c0f3
EdwardWebDev <edwardethridge1@gmail.com>
2021-10-24 05:20:15 -0400

    reworking logic

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
 const computerPlay = function () {
     let move = Math.floor((Math.random() * 3) + 1)
     if (move === 1) {
-        return console.log('ROCK')
+        return 'ROCK'
     } else if (move === 2) {
-        return console.log('PAPER')
+        return 'PAPER'
     } else {
-        return console.log('SCISSORS')
+        return 'SCISSORS'
     }
 }
+
+// const rockPaperScissors = function (playerSelection, computerSelection) {
+//     if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+//         return 'You Lose!'
+//     } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+//         return 'You win!'
+//     } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+//         return 'You lose!'
+//     } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+//         return 'You win!'
+//     } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+//         return 'You lose!'
+//     } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+//         return 'You win!'
+//     } else {
+//         return 'A draw!'
+//     }
+// }
 
 const computerSelection = computerPlay();
 const input = prompt('Please choose rock, paper or scissors.')
 const playerSelection = input.toUpperCase();
-console.log(playerSelection);
 
-const rockPaperScissors = function (playerSelection, computerSelection) {
-    if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return 'You Lose!'
-    } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-        return 'You win!'
-    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return 'You lose!'
-    } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-        return 'You win!'
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return 'You lose!'
-    } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return 'You win!'
-    } else {
-        return 'A draw!'
-    }
+const game = function () {
+    rockPaperScissors();
+    rockPaperScissors();
+    rockPaperScissors();
+    rockPaperScissors();
+    rockPaperScissors();
 }
 
-const game = function() {
-    let playerScore = 0;
-    let computerScore = 0;
-    while(playerScore !== 5 && computerScore !== 5){
-        rockPaperScissors(playerSelection, computerSelection)
-    }
-}
+game();
